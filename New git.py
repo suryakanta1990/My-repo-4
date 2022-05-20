d7e85f174cfec3c8195d674dfb1e4c127d862c0e
suryakanta1990 <105774247+suryakanta1990@users.noreply.github.com>
2022-05-20 23:55:31 +0530

    new selenium comand import function

--- New git.py
+++ New git.py
@@ -1,1 +1,9 @@
-from selenium import webdriver
+List = [100, 2, 300, 10, 11, 1000]
+
+largest_number = List[0]
+
+for a in List:
+    if a > largest_number:
+        largest_number = a
+
+print(largest_number)
